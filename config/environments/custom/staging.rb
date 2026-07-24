1684cc0460bc78d777cb440e1cc1a32aa56d3f55
Csaba Csuzdi <csuzdy@gmail.com>
2026-07-25 00:22:58 +0200

    Fix asset compile error

--- config/environments/custom/staging.rb
+++ config/environments/custom/staging.rb
@@ -1,7 +1,3 @@
 Rails.application.configure do
-  # Overwrite settings for the staging environment or add your own
-  # custom settings for this environment. Note these changes will
-  # also affect the preproduction environment, so edit the
-  # `config/environments/custom/preproduction.rb` file if you don't
-  # want these changes to be applied there as well.
+  config.assets.js_compressor = false
 end
